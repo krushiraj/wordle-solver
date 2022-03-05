3cceed7663fd7388c83b99bdab148b4fd17ce1b7
Krushi Raj Tula <krushiraj123@gmail.com>
2022-03-05 21:24:02 +0530

    feat: main game and ds logic

---
+++ game.py
@@ -0,0 +1,121 @@
+import os
+from random import random
+import string
+from random import randint
+from utils.constants import CORRECT, REPEATED, VALID, INVALID
+from utils.words import total_num_words, words
+from tries import get_tries_root
+
+DEBUG = os.environ.get('DEBUG', False);
+
+game_state = [[[None, None] for j in range(5)] for i in range(6)]
+total_chances = 6
+letters = zip(string.ascii_lowercase, [
+              VALID for i in range(0, len(string.ascii_lowercase))])
+words_root = get_tries_root()
+
+
+def get_first_word():
+    return words[randint(0, total_num_words - 1)]
+
+
+def get_word(current_state, is_first_word):
+    if is_first_word:
+        return get_first_word()
+
+    word_so_far, valid_chars = update_state_and_graph(current_state)
+    print('Correct Letters in word so far: {}'.format(' '.join(word_so_far)))
+    return words_root.get_word(word_so_far, valid_chars)
+
+
+def update_state_and_graph(current_state):
+    word_so_far = []
+    invalid_chars = []
+    invalid_by_position = []
+    valid_chars = []
+    idx = 0
+    for letter, validity in current_state:
+        if validity == CORRECT:
+            word_so_far.append(letter)
+        else:
+            word_so_far.append('_')
+
+        if validity == INVALID:
+            invalid_chars.append(letter)
+        elif validity == VALID:
+            valid_chars.append(letter)
+            invalid_by_position.append((letter, idx))
+        idx += 1
+
+    words_root.invalidate_chars(invalid_chars)
+    words_root.invalidate_chars_by_position(invalid_by_position)
+    return word_so_far, valid_chars
+
+
+def guess_word(current_state):
+    print('Guessing the words')
+    is_first_word = current_state is None
+    word = get_word(current_state, is_first_word)
+    print('Word: {}'.format(word.upper()))
+    print('\n')
+    return word
+
+
+def check_end_game(idx):
+    for char, validity in game_state[idx]:
+        if validity != CORRECT:
+            return False
+    return True
+
+
+def clean_result(result, word):
+    final_result = [None for i in range(len(word))]
+    repeated_chars = []
+    for i in range(len(word)):
+        if result[i] == INVALID and word[i] in word[:i]:
+            final_result[i] = REPEATED
+            repeated_chars.append(word[i])
+        else:
+            final_result[i] = result[i]
+    words_root.do_not_repeat_chars(repeated_chars)
+    return final_result
+
+
+def run_game_loop(chances=total_chances):
+    print('Game loop started')
+    for chance_num in range(chances):
+        print('Chance number: {}'.format(chance_num))
+        if DEBUG:
+          print('Game state: {}'.format(game_state))
+          print('\n')
+
+        word = guess_word(current_state=None if chance_num ==
+                          0 and chances == total_chances else game_state[chance_num-1])
+
+        result = clean_result(list(map(int, input(
+            'Enter the result of the word: (GREY = 0, YELLOW = 1, GREEN = 2): ')[:5])), word)
+
+        game_state[chance_num] = [[word[i], result[i]]
+                                  for i in range(len(word))]
+        print('\n')
+        if check_end_game(chance_num):
+            print('Game over')
+            break
+    return
+
+
+def load_game_state():
+    print('Loading game state')
+    chances = int(
+        input('Enter the number of chances you have already played: '))
+    chances_left = total_chances - chances
+
+    for i in range(chances):
+        print('Chance number: {}'.format(i+1))
+        word = input('Enter the word: ')
+        result = clean_result(list(map(int, input(
+            'Enter the result of the word: (GREY = 0, YELLOW = 1, GREEN = 2): ')[:5])), word)
+        game_state[i] = [[word[i], result[i]] for i in range(len(word))]
+        update_state_and_graph(game_state[i])
+        print('\n')
+    return chances_left
---
+++ tries.py
@@ -0,0 +1,135 @@
+from random import randint, shuffle
+from sort import sort_words_by_vowel_count
+from utils.constants import VALID, INVALID
+from utils.words import words
+
+
+class Node:
+    def __init__(self, char):
+        self.char = char
+        self.validity = VALID
+        self.allow_repeat = True
+        self.next = [None for i in range(26)]
+
+
+class TrieCollection:
+    def __init__(self):
+        self.tries = [None for j in range(26)]
+
+    def build_tries(self):
+        for word in words:
+            self.add_word(word)
+
+    def add_word(self, word, prev_node=None):
+        if len(word) == 0:
+            return
+
+        position = ord(word[0]) - ord('a')
+        current_node = None
+        if prev_node is None:
+            if self.tries[position] is None:
+                self.tries[position] = Node(word[0])
+            current_node = self.tries[position]
+        else:
+            current_node = prev_node.next[position] if prev_node.next[position] else Node(
+                word[0])
+            prev_node.next[position] = current_node
+
+        self.add_word(word[1:], current_node)
+
+    def do_not_repeat_char(self, char, node=None):
+        if node is None:
+            for i in range(len(self.tries)):
+                if self.tries[i] is not None:
+                    self.do_not_repeat_char(char, self.tries[i])
+            return
+
+        if node.char == char:
+            node.allow_repeat = False
+        for i in range(len(node.next)):
+            if node.next[i] is not None:
+                self.do_not_repeat_char(char, node.next[i])
+
+    def do_not_repeat_chars(self, chars):
+        for char in chars:
+            self.do_not_repeat_char(char)
+
+    def invalidate_char(self, char, node=None):
+        if node is None:
+            for i in range(len(self.tries)):
+                if self.tries[i] is not None:
+                    self.invalidate_char(char, self.tries[i])
+            return
+
+        if node.char == char:
+            node.validity = INVALID
+        for i in range(len(node.next)):
+            if node.next[i] is not None:
+                self.invalidate_char(char, node.next[i])
+
+    def invalidate_chars(self, chars):
+        for char in chars:
+            self.invalidate_char(char)
+
+    def invalidate_char_by_position(self, char, position, curr_position, node=None):
+        if node is None:
+            for i in range(len(self.tries)):
+                if self.tries[i] is not None:
+                    self.invalidate_char_by_position(
+                        char, position, curr_position + 1, self.tries[i])
+            return
+
+        if node.char == char and curr_position == position:
+            node.validity = INVALID
+            return
+
+        for i in range(len(node.next)):
+            if node.next[i] is not None:
+                self.invalidate_char_by_position(
+                    char, position, curr_position+1, node.next[i])
+
+    def invalidate_chars_by_position(self, char_position_tuples):
+        for char, position in char_position_tuples:
+            self.invalidate_char_by_position(char, position, -1)
+
+    def get_word(self, word_so_far, letters_present_in_word):
+        possible_words = []
+        self.get_possible_words(
+            word_so_far, '', 0, self.tries, letters_present_in_word, possible_words)
+        return sort_words_by_vowel_count(possible_words)[0]
+
+    def has_expected_letters(self, possibility, letters_present_in_word):
+        possiblity_chars = list(possibility)
+        for char in letters_present_in_word:
+            if char not in possiblity_chars:
+                return False
+            index = possiblity_chars.index(char)
+            # this helps to be sure about repeating letters in answer
+            possiblity_chars = possiblity_chars[:index] + \
+                possiblity_chars[index + 1:]
+        return True
+
+    def get_possible_words(self, word_to_match, possibility, idx, current_node, letters_present_in_word, word_list):
+        if current_node is None:
+            return
+
+        if idx == len(word_to_match) and self.has_expected_letters(possibility, letters_present_in_word):
+            word_list.append(possibility)
+            return
+
+        for i in range(26):
+            if current_node[i] is not None and current_node[i].validity == VALID:
+                if word_to_match[idx] == '_':
+                    if not current_node[i].allow_repeat and current_node[i].char in possibility:
+                        continue
+                    self.get_possible_words(
+                        word_to_match, possibility + current_node[i].char, idx + 1, current_node[i].next if current_node[i] is not None else None, letters_present_in_word, word_list)
+                elif word_to_match[idx] == current_node[i].char:
+                    self.get_possible_words(
+                        word_to_match, possibility + current_node[i].char, idx + 1, current_node[i].next if current_node[i] is not None else None, letters_present_in_word, word_list)
+
+
+def get_tries_root():
+    root = TrieCollection()
+    root.build_tries()
+    return root
