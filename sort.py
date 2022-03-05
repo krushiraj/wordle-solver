b4051e3c129de496f19f061e56edda5a333fbac0
Krushi Raj Tula <krushiraj123@gmail.com>
2022-03-05 21:23:43 +0530

    feat: add utils functions and word list

---
+++ sort.py
@@ -0,0 +1,6 @@
+def vowel_count(word):
+    return sum([1 for char in word if char in 'aeiou'])
+
+
+def sort_words_by_vowel_count(words):
+    return sorted(words, key=lambda x: (-vowel_count(set(x)), x))
---
+++ utils/constants.py
@@ -0,0 +1,7 @@
+REPEATED, INVALID, VALID, CORRECT = range(-1, 3)
+
+color_to_validity = {
+    'GREY': INVALID,
+    'YELLOW': VALID,
+    'GREEN': CORRECT
+}
